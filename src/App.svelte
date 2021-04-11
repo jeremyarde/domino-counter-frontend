486dde2a418812cefd33cc79272c70b9762bee02
Jeremy <jere.arde@gmail.com>
2021-04-10 23:42:41 -0500

    Fix cropping on front end, add drawing square around cropped location

--- src/App.svelte
+++ src/App.svelte
@@ -3,44 +3,121 @@
 
   let fileinput = null;
   let result = 0;
-  let width;
-  let height;
-  let found_domino_string = "DOMINO HERE";
-  let top_left;
-  let bottom_right;
+
+  let found_domino_string = "Please upload photo";
   let currentImage;
   let canvas;
   let edgePositions = [];
+
+  let currentDominoImage = null;
+  let currentDominoImageHeight = 0;
+  let currentDominoImageWidth = 0;
+  let currentDominoImageCropSection = null;
 
   function handleMousemove(event) {
     console.log(canvas.getBoundingClientRect());
     let canvas_rectangle = canvas.getBoundingClientRect();
 
     var context = canvas.getContext("2d");
-    context.clearRect(0,0, canvas.width, canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height);
     canvas.width = currentImage.naturalWidth;
     canvas.height = currentImage.naturalHeight;
     context.drawImage(currentImage, 0, 0);
 
     let x = event.clientX - canvas_rectangle.left;
-		let y = event.clientY - canvas_rectangle.top;
-    edgePositions.push({"x": x, "y":y});
+    let y = event.clientY - canvas_rectangle.top;
+    edgePositions.push({ x: x, y: y });
 
     if (edgePositions.length > 2) {
-      edgePositions.shift()
+      edgePositions.shift();
     }
 
-    edgePositions.forEach(pos => {
+    // draw points
+    edgePositions.forEach((pos) => {
       console.log(pos.x);
       console.log(pos.y);
-      context.fillStyle = "#000000";
+      context.fillStyle = "#3aeb34";
       context.beginPath();
-      context.arc(pos.x, pos.y, 20, 0, 2*Math.PI);
+      context.arc(pos.x, pos.y, 20, 0, 2 * Math.PI);
       context.fill();
     });
 
-    console.log(`${x}, ${y }`);
-	}
+    // draw rectangle through points
+    let topLeft;
+    let bottomRight;
+    if (edgePositions.length == 2) {
+      if (edgePositions[0].x < edgePositions[1].x) {
+        topLeft = edgePositions[0];
+        bottomRight = edgePositions[1];
+      } else {
+        topLeft = edgePositions[1];
+        bottomRight = edgePositions[0];
+      }
+      context.strokeStyle = "green";
+      context.lineWidth = 5;
+      context.shadowColor = "#d53";
+      currentDominoImageCropSection = {
+        x: topLeft.x,
+        y: topLeft.y,
+        x_len: bottomRight.x - topLeft.x,
+        y_len: bottomRight.y - topLeft.y,
+      };
+      context.strokeRect(
+        currentDominoImageCropSection["x"],
+        currentDominoImageCropSection["y"],
+        currentDominoImageCropSection["x_len"],
+        currentDominoImageCropSection["y_len"]
+      );
+    }
+
+    console.log("domino image section (JS)");
+    console.log(currentDominoImageCropSection);
+    console.log(`${x}, ${y}`);
+  }
+
+  function onSubmitPicture(event) {
+    console.log("onSubmitPicture");
+    // send the image data to wasm for counting
+    found_domino_string = "waiting...";
+    let wasm_result;
+
+    if (currentDominoImage != null) {
+      if (currentDominoImageCropSection != null) {
+        var context = canvas.getContext("2d");
+        var imageData = context.getImageData(
+          currentDominoImageCropSection["x"],
+          currentDominoImageCropSection["y"],
+          currentDominoImageCropSection["x_len"],
+          currentDominoImageCropSection["y_len"]
+        ).data;
+
+        console.log("domino image section (JS)");
+        console.log(currentDominoImageCropSection);
+
+        wasm_result = count_dominoes(
+          imageData,
+          currentDominoImageCropSection["x_len"],
+          currentDominoImageCropSection["y_len"],
+          true
+        );
+      } else {
+        wasm_result = count_dominoes(
+          currentDominoImage,
+          currentDominoImageWidth,
+          currentDominoImageHeight,
+          false
+        );
+      }
+      console.log("Results of count:");
+      // console.log(result);
+      result = wasm_result["value"];
+      console.log(wasm_result);
+      found_domino_string = wasm_result["string_rep"];
+      console.log("after return");
+    } else {
+      alert("No picture uploaded yet.");
+    }
+  }
 
   const onFileSelected = (e) => {
     const ctx = canvas.getContext("2d");
@@ -59,43 +136,49 @@
         // img.style.display = "none";
 
         var myData = ctx.getImageData(0, 0, img.width, img.height);
-        console.log("data:");
-        console.log(myData.data);
 
-        // send the image data to wasm for counting
-        // result = count_dominoes(
-        //   myData.data,
-        //   img.naturalWidth,
-        //   img.naturalHeight
-        // );
-        // console.log("Results of count:");
-        // console.log(result);
+        console.log("Image set. Ready to analyze.");
+        // console.log(myData);
+        currentDominoImage = myData.data;
+        currentDominoImageWidth = img.width;
+        currentDominoImageHeight = img.height;
       };
       img.src = event.target.result;
     };
     reader.readAsDataURL(e.target.files[0]);
   };
-
-  
-
 </script>
 
 <main>
   <div>
-    <h2>Domino count: {result}</h2>
-    <h3>Found dominoes: {found_domino_string}</h3>
+    <input
+      type="file"
+      accept="image/*"
+      on:change={(e) => onFileSelected(e)}
+      bind:this={fileinput}
+    />
   </div>
-<div>
-  <input
-    type="file"
-    accept="image/*"
-    capture="environment"
-    on:change={(e) => onFileSelected(e)}
-    bind:this={fileinput}
+  <canvas
+    bind:this={canvas}
+    width={32}
+    height={32}
+    on:click={handleMousemove}
   />
-</div>
-  <canvas bind:this={canvas} width={32} height={32} on:click={handleMousemove} />
-
+  <div>
+    <button on:click={onSubmitPicture}>ANALYZE PICTURE</button>
+    <h2>Count: {result}<br />Dominoes Found: {found_domino_string}</h2>
+  </div>
+  <h2 style="text-align: left">
+    Instructions:<br />
+  </h2>
+  <p style="text-align: left">
+    1. Arrange the dominoes in a single file vertical line<br />
+    2. Click on the "Browse..." button to and take a photo of the dominoes from directly
+    above<br />
+    3. Tap on the top left and top right corner of the line of dominoes to crop the
+    image (you don't have to be too precise)<br />
+    4. Click the "Analyze" button
+  </p>
 </main>
 
 <style>
